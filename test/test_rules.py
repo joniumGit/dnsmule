a7201d8727df10e2a204072e0267a39438752e2f
joniumGit <52005121+joniumGit@users.noreply.github.com>
2023-03-02 13:18:44 +0200

    Adds tests for rules class

--- test/test_rules.py
+++ test/test_rules.py
@@ -34,6 +34,106 @@ def test_rules_rule_unknown_type(rules):
     assert "name='unknown_type'" in str(rules[unknown].pop())
 
 
+def test_rules_iter_returns_items_from_rules(rules):
+    marker = object()
+    rules._rules[RRType.TXT] = marker
+
+    assert next(iter(rules)) == (RRType.TXT, marker), 'Did not return the expected iteration result'
+
+
+def test_rules_getitem_returns_items_from_rules(rules):
+    marker = object()
+    rules._rules[RRType.TXT] = marker
+
+    assert rules[RRType.TXT] is marker, 'Did not return from rules'
+
+
+def test_rules_getitem_returns_items_from_rules_conversion(rules):
+    marker = object()
+    rules._rules[RRType.A] = marker
+
+    assert rules[1] is marker, 'Did not convert int'
+    assert rules['a'] is marker, 'Did not convert lower string'
+    assert rules['A'] is marker, 'Did not convert upper string'
+
+
+def test_rules_get_types_returns_rules_keys(rules):
+    marker = object()
+    marker2 = object()
+
+    rules._rules[marker] = 1
+    rules._rules[marker2] = 2
+
+    types = iter(rules.get_types())
+    assert next(types) is marker, 'Unexpected return or order'
+    assert next(types) is marker2, 'Unexpected return or order'
+
+
+@async_test
+async def test_rules_process_error(rules):
+    class MockLog:
+
+        def __getattr__(self, item):
+            assert item == 'error', 'Unexpected attribute'
+            return self.__call__
+
+        def __call__(self, message, *args, **kwargs):
+            assert len(args) == 0, 'Too many args'
+            assert message is not None and isinstance(kwargs['exc_info'], ValueError), 'Arguments did not match'
+
+    rules.log = MockLog()
+
+    rules.add_rule(RRType.TXT, Rule(f=lambda *_, **__: exec('raise ValueError()')))
+
+    res = await rules.process_record(Record(domain='example.com', data='', type=RRType.TXT))
+    assert res is not None, 'Did not return result'
+    assert not res, 'Did not return empty result'
+
+
+@async_test
+async def test_rules_process_async_rule(rules):
+    class MockRule(Rule):
+        called = set()
+
+        def __call__(self, *args, **kwargs):
+            self.called.add('__call__')
+            return self
+
+        def __getattr__(self, item):
+            self.called.add(item)
+
+        def __await__(self):
+            self.called.add('__await__')
+
+    rules.add_rule(RRType.TXT, MockRule())
+
+    await rules.process_record(Record(domain='example.com', data='', type=RRType.TXT))
+
+    assert MockRule.called == {'__call__', '__await__'}, 'Not all methods were called'
+
+
+@async_test
+async def test_rules_process_normal_rule(rules):
+    class MockRule(Rule):
+        called = set()
+
+        def __call__(self, *args, **kwargs):
+            self.called.add('__call__')
+            return None
+
+        def __getattr__(self, item):
+            self.called.add(item)
+
+        def __await__(self):
+            assert False, 'Should not be called'
+
+    rules.add_rule(RRType.MX, MockRule())
+
+    await rules.process_record(Record(domain='example.com', data='', type=RRType.MX))
+
+    assert MockRule.called == {'__call__'}, 'Not all or too many methods were called'
+
+
 def test_rules_factory_operation(rules):
     @rules.register('test')
     def my_cool_rule(name: str, **__):
@@ -84,7 +184,7 @@ async def test_rules_dynamic_factory_operation(rules):
     assert next(iter(result.tags)) == 'True'
 
 
-def test_create_regex_rule(rules):
+def test_rules_create_regex_rule(rules):
     rule = rules.create_rule({
         'type': 'dns.regex',
         'name': 'test',

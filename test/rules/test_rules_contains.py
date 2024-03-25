a85193e50858ff6b85c1ceb9d8ca80b1e3a64e21
joniumGit <52005121+joniumGit@users.noreply.github.com>
2024-03-25 23:25:00 +0200

    Adds simple test for contains

---
+++ test/rules/test_rules_contains.py
@@ -0,0 +1,45 @@
+from dnsmule import ContainsRule, Record, Result, RRType, Domain
+
+
+def test_contains_rule_appends_tag_to_result_on_matching():
+    rule = ContainsRule(identities=[{
+        'value': 'a.example',
+        'name': 'example',
+        'type': 'CDN'
+    }])
+
+    record = Record(
+        name=Domain('example.com'),
+        type=RRType.CNAME,
+        data='a.example.com',
+    )
+
+    result = Result(
+        name=Domain('example.com'),
+    )
+
+    rule(record, result)
+
+    assert result.tags == {'DNS::CIDER::CDN::EXAMPLE'}
+
+
+def test_contains_rule_does_nothing_on_not_matching():
+    rule = ContainsRule(identities=[{
+        'value': 'a.example',
+        'name': 'example',
+        'type': 'CDN'
+    }])
+
+    record = Record(
+        name=Domain('example.com'),
+        type=RRType.CNAME,
+        data='b.example.com',
+    )
+
+    result = Result(
+        name=Domain('example.com'),
+    )
+
+    rule(record, result)
+
+    assert not result.tags
